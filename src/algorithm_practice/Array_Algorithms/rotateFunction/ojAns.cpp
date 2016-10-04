21da9c7a29e6921909ce2523557352d5bf2d700e
Dom Farolino <domfarolino@gmail.com>
2016-10-03 23:22:32 -0400

    algo(Array_Algorithms + rotateFunction): added rotateFunction, documentation, and online judge solution file

---
+++ src/algorithm_practice/Array_Algorithms/rotateFunction/ojAns.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  int maxRotateFunction(const vector<int>& A) {
+    int currFValue = 0, sum = 0, maxFValue;
+    
+    // Set F(0) and sum
+    for (int i = 0; i < A.size(); ++i) {
+      currFValue += i*A[i];
+      sum += A[i];
+    }
+
+    maxFValue = currFValue;
+
+    for (int i = 0; i < A.size(); ++i) {
+      currFValue -= sum;
+      currFValue += A.size()*A[i];
+      maxFValue = max(currFValue, maxFValue);
+    }
+
+    return maxFValue;
+  }
+};
