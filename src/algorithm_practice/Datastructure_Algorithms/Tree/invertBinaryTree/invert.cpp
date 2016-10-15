e7efb625ad21e7e5eebe1875c39c758143a0f641
domfarolino <domfarolino@gmail.com>
2016-10-15 00:46:44 -0400

    algo(invertBinaryTree): added iterative BFS solution + doc

--- src/algorithm_practice/Datastructure_Algorithms/Tree/invertBinaryTree/invert.cpp
+++ src/algorithm_practice/Datastructure_Algorithms/Tree/invertBinaryTree/invert.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -12,6 +13,13 @@ using namespace std;
  * Time complexity: O(n) - since we have to touch all of the nodes
  * Space complexity: O(h) - where h is the height of the tree. This
  * value could be O(log(n)) if the tree is complete or perfect.
+ *
+ * The iterative version of this algorithm is a basic breadth first search.
+ * We want to store all of the nodes in a queue, and for each node, swap its
+ * children.
+ * Time complexity: O(n) - since we have to touch all of the nodes
+ * Space complexity: O(n) - since worst case we have to store all of
+ * the nodes in the queue technically
  */
 
 template <typename T>
@@ -30,6 +38,8 @@ void deleteTree(TreeNode<T>* node) {
   delete node;
   node = NULL;
 }
+
+//////////////// Recursive (DFS) version ////////////////
 
 // Time complexity: O(n)
 // Space complexity: O(h) where h = height of tree
@@ -51,6 +61,36 @@ TreeNode<T>* invertBinaryTree(TreeNode<T>* root) {
   return root;
 }
 
+//////////////// Iterative (BFS) version ////////////////
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+template <typename T>
+TreeNode<T>* invertBinaryTreeIterative(TreeNode<T>* root) {
+  queue<TreeNode<T>*> q;
+  TreeNode<T>* tmpNode; // store this up here so we don't keep throwing it on and off the stack
+
+  if (root) q.push(root);
+
+  while (q.size()) {
+    if (q.front()->left) {
+      q.push(q.front()->left);
+    }
+
+    if (q.front()->right) {
+      q.push(q.front()->right);
+    }
+
+    tmpNode = q.front()->left;
+    q.front()->left = q.front()->right;
+    q.front()->right = tmpNode;
+
+    q.pop();
+  }
+
+  return root;
+}
+
 int main() {
   TreeNode<int> *root = new TreeNode<int>(1);
   root->left = new TreeNode<int>(2);
@@ -58,7 +98,7 @@ int main() {
 
   cout << "root: " << root->val << "\nleft: " << root->left->val << "\nright: " << root->right->val << endl;
 
-  root = invertBinaryTree(root);
+  root = invertBinaryTreeIterative(root);
 
   cout << "root: " << root->val << "\nleft: " << root->left->val << "\nright: " << root->right->val << endl;
 
