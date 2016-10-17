73cf8dfc6bc5c10ed038129392039d1f457810e7
Sunil Sharma <sunilsharma.ss08@gmail.com>
2016-10-17 22:46:16 +0530

    algo: radix sort implementation

--- src/algorithm_practice/Searching_And_Sorting/RadixSort/RadixSort.cpp
+++ src/algorithm_practice/Searching_And_Sorting/RadixSort/RadixSort.cpp
@@ -1,69 +1,101 @@
-//
-//  RadixSort
-//
-//  Created by Sunil Sharma on 09/10/16.
-//  Copyright © 2016 Sunil Sharma. All rights reserved.
-//
 
-#include <assert.h>
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
+/**
+ The idea of Radix Sort is to do digit by digit sort starting from least significant digit to most significant digit. Radix sort uses counting sort as a subroutine to sort.
+ 
+ Time Complexity:
+ Let there be d digits in input integers. Radix Sort takes O(d*(n+b)) time where b is the base for representing numbers, for example, for decimal system, b is 10. What is the value of d? If k is the maximum possible value, then d would be O(logb(k)). So overall time complexity is O((n+b) * logb(k)). Which looks more than the time complexity of comparison based sorting algorithms for a large k. Let us first limit k. Let k <= nc where c is a constant. In that case, the complexity becomes O(nLogb(n)). But it still doesn’t beat comparison based sorting algorithms.
+ What if we make value of b larger?. What should be the value of b to make the time complexity linear? If we set b as n, we get the time complexity as O(n). In other words, we can sort an array of integers with range from 1 to nc if the numbers are represented in base n (or every digit takes log2(n) bits).
+ 
+ The Radix Sort Algorithm
+ 1) Do following for each digit i where i varies from least significant digit to the most significant digit.
+ ………….a) Sort input array using counting sort (or any stable sort) according to the i’th digit.
+ 
+ Example:
+ Original, unsorted list:
+ 
+ 170, 45, 75, 90, 802, 24, 2, 66
+ Sorting by least significant digit (1s place) gives: [*Notice that we keep 802 before 2, because 802 occurred before 2 in the original list, and similarly for pairs 170 & 90 and 45 & 75.]
+ 
+ 170, 90, 802, 2, 24, 45, 75, 66
+ Sorting by next digit (10s place) gives: [*Notice that 802 again comes before 2 as 802 comes before 2 in the previous list.]
+ 
+ 802, 2, 24, 45, 66, 170, 75, 90
+ Sorting by most significant digit (100s place) gives:
+ 
+ 2, 24, 45, 66, 75, 90, 170, 802
+ 
+ */
 
-#include <algorithm>
-#include <iostream>
-#include <vector>
+// C++ implementation of Radix Sort
+#include<iostream>
+using namespace std;
 
-void radixSort(int array[], int len) {
-    const int COLUMS = len + 1;
-    int *storer = new int[10 * COLUMS];
-    bool bNoLargerInt = false;
-    int checkingRadix = 0;
-    while (!bNoLargerInt) {
-        bNoLargerInt = true;
-        for (int i = 0; i < len; i ++) {
-            int leftValueAfterMuti = (array[i] / pow(10, checkingRadix));
-            int theRadix = leftValueAfterMuti % 10;
-            if (leftValueAfterMuti != 0) {
-                bNoLargerInt = false;
-            }
-            storer[theRadix * COLUMS + storer[theRadix * COLUMS] + 1] = array[i];
-            storer[theRadix * COLUMS] ++;
-        }
-        checkingRadix ++;
-        
-        //set the newly sorted result backto 'array'
-        int cursor = 0;
-        for (int i = 0; i < 10; i ++) {
-            for (int j = 1; j <= storer[i * COLUMS]; j ++) {
-                array[cursor] = storer[i * COLUMS + j];
-                cursor ++;
-            }
-            //reset the adding point of every row to 1, that means adding element to row will be started from 1, rather than 0, becuse at 0 there is the adding point record.
-            storer[i * COLUMS] = 0;
-        }
-    }
+// A utility function to get maximum value in arr[]
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
 }
 
-int main() {
-    srand(time(NULL));
-    const int TESTED_ARRAY_LEN = 1000;
-    const int INT_RANGE_BELOW = 100000;
-    int *array = new int[TESTED_ARRAY_LEN];
-    for (int i = 0; i < TESTED_ARRAY_LEN; i ++) {
-        array[i] = rand() % INT_RANGE_BELOW;
+// A function to do counting sort of arr[] according to
+// the digit represented by exp.
+void countSort(int arr[], int n, int exp)
+{
+    int output[n]; // output array
+    int i, count[10] = {0};
+    
+    // Store count of occurrences in count[]
+    for (i = 0; i < n; i++)
+        count[ (arr[i]/exp)%10 ]++;
+    
+    // Change count[i] so that count[i] now contains actual
+    //  position of this digit in output[]
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+    
+    // Build the output array
+    for (i = n - 1; i >= 0; i--)
+    {
+        output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+        count[ (arr[i]/exp)%10 ]--;
     }
     
-    // get the result through our way.
-    radixSort(array, TESTED_ARRAY_LEN);
+    // Copy the output array to arr[], so that arr[] now
+    // contains sorted numbers according to current digit
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+// The main function to that sorts arr[] of size n using
+// Radix Sort
+void radixsort(int arr[], int n)
+{
+    // Find the maximum number to know number of digits
+    int m = getMax(arr, n);
     
-    // get the result through the standard way.
-    std::vector<int> v(array, array + TESTED_ARRAY_LEN);
-    std::sort(v.begin(), v.end());
-    
-    // compare them
-    for (int i = 0; i < TESTED_ARRAY_LEN; i ++) {
-        assert(v[i] == array[i]);
-        std::cout << array[i] << ", ";
-    }
+    // Do counting sort for every digit. Note that instead
+    // of passing digit number, exp is passed. exp is 10^i
+    // where i is current digit number
+    for (int exp = 1; m/exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+
+// A utility function to print an array
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
+
+// Driver program to test above functions
+int main()
+{
+    int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    radixsort(arr, n);
+    print(arr, n);
+    return 0;
 }
