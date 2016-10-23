f3276796d6be025be85fe36a3e0e2df2a8cc583b
domfarolino <domfarolino@gmail.com>
2016-10-23 19:16:32 -0400

    algo(sockMerchant): added trivial sock merchant pairing algorithm and documentation

---
+++ src/algorithm_practice/Number_Algorithms/sockMercant/sock.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+// Source: https://www.hackerrank.com/challenges/sock-merchant
+
+/**
+ * Sock Merchant
+ * This problem is basically the same as
+ * finding the number of pairs of identical items
+ * in a list. The max length for each list is 100 and
+ * the max number of items in each list is 100. We can read
+ * in the entire list, and create a "map", or simple integer
+ * array to act as a map. We can increment each value in the array
+ * to denote the number of times some item appears in the original list.
+ * We then can go through the entire map, and we know we can sell list[i]/2
+ * pairs of an item for all items in the list. list[i]/2 makes sense here because
+ * if have list[i] = 2, we can only sell one pair, or list[i]/2. If list[i] = 3, we
+ * still can only sell floor(3/2) = 1 pairs.
+ */
+
+int main() {
+  int n, tmpItem;
+  int list[101] = {0};
+  cin >> n;
+  for (int i = 0; i < n; ++i) {
+    cin >> tmpItem;
+    list[tmpItem]++;
+  }
+
+  int totalPairs = 0;
+  for (int i = 0; i < 101; ++i) {
+    totalPairs += list[i]/2;
+  }
+
+  cout << totalPairs << endl;
+
+  return 0;
+}
