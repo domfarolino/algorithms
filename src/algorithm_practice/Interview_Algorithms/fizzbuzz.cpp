12a38add61fe2c985cd0514c308fb05e58afcbe0
jmeline <jacob.meline@gmail.com>
2016-10-23 00:37:14 -0600

    fix(cpp-fizzbuzz): fixed c++ styling issues

--- src/algorithm_practice/Interview_Algorithms/fizzbuzz.cpp
+++ src/algorithm_practice/Interview_Algorithms/fizzbuzz.cpp
@@ -2,9 +2,10 @@
 
 /*
  * Fizz Buzz
- * The "Fizz-Buzz test" is an interview question designed to help filter out the
- * 99.5% of programming job candidates who can't seem to program their way out of
- * a wet paper bag.
+ * The "Fizz-Buzz test" is an interview question
+ * designed to help filter out the 99.5% of programming
+ * job candidates who can't seem to program their way out
+ * of a wet paper bag.
  *
  * The text of the programming assignment is as follows:
  * "Write a program that prints the numbers from 1 to 100.
@@ -13,25 +14,24 @@
  * For numbers which are multiples of both three and five print “FizzBuzz”."
  *
  */
-bool isDivisibleByX (int number, int divider) {
+bool isDivisibleByX(int number, int divider) {
   return number % divider == 0;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   int maxRange;
   std::cout << "Input a range" << std::endl;
   std::cin >> maxRange;
 
   for (int i = 0; i < maxRange; i++) {
     std::cout << i << ": ";
-    if (isDivisibleByX(i, 3)){
+    if (isDivisibleByX(i, 3)) {
       std::cout << "fizz";
     }
     if (isDivisibleByX(i, 5)) {
       std::cout << "buzz";
     }
-
     std::cout << std::endl;
   }
+  return 0;
 }
