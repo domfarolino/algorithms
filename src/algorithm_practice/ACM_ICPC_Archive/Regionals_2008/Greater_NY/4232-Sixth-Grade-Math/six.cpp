e5939472d208977dab7cdfe4bc148749f77eac59
Dom Farolino <domfarolino@gmail.com>
2016-11-24 12:55:03 -0500

    algo(4232-Sixth-Grade-Math): finished documentation and algorithm

--- src/algorithm_practice/ACM_ICPC_Archive/Regionals_2008/Greater_NY/4232-Sixth-Grade-Math/six.cpp
+++ src/algorithm_practice/ACM_ICPC_Archive/Regionals_2008/Greater_NY/4232-Sixth-Grade-Math/six.cpp
@@ -9,55 +9,53 @@ using namespace std;
  * of two given numbers, and the other to compute the greatest common
  * factor (divisor) of two given numbers.
  *
- * When doing this algorithm I started with the LCM. Working out a few examples
- * by hand revealed that the range of the LCM of two numbers m, n where m <= n is [n, m*n].
+ * When doing this algorithm I started with the LCM. Working out a few examples by
+ * hand revealed that the range of the LCM of two numbers m, n where m <= n is [n, m*n].
  * In other words the smallest value the LCM of m and n can take is no smaller than the larger
- * of the two numbers (obvious) and the largest value the LCM of m and n can take is no larger than
- * m*n. It should be clear that we can start at m*n (a guaranteed common multiple) and then see if it
- * is possible to trim that number down a bit. So how do we make some common multiple the least common
- * multiple? A few examples on paper made it clear that trimming m*n down meant getting rid of shared factor
- * produced when multiplying together. For example with the numbers 2 and 16, multiplying gives us 32 however
- * since both 2 and 16 share a common factor (2), 32 will contain the shared factor of 2 twice! With values 6 and
- * 8, 6*8 = 48 which is a common multiple of both 6 and 8 however it is not the least common multiple. We could divide
- * 48/2 and be left with a smaller common multiple. This is because both 6 and 8 share a factor of 2 so we can get rid of
- * one of the two duplicate factors by dividing once by two. Let's take a look at one more example: 4, 22. 4*22 = 88 and like
- * before we could divide 88/2 since both values share a factor of two, however we'd have to divide our result (44) by 2 again
- * to get the least common multiple. This is because 4 and 22 share the factor of 2 twice, so we'd either have to divide 88 by
+ * of the two numbers (obvious) and the largest value the LCM can take is no larger than m*n. It
+ * should be clear that we can start at m*n (a guaranteed common multiple) and then see if we can
+ * trim it down a bit. So how do we make some common multiple the least common multiple? A few examples
+ * on paper made it clear that trimming m*n down meant getting rid of shared factors produced when multiplying
+ * m*n. For example with the numbers 2 and 16, 2*16 = 32. However, since 2 and 16 share a common factor 2, 32 will
+ * contain the shared factor of 2 twice! We can get rid of the duplicate by dividing by 2 once. The logic is similar
+ * to the inclusion-exclusion principle. Another example: 8, 28. 8*28 = 224 and like before we COULD divide 224/2 since
+ * both values share a factor of 2, however we'd need to divide by 2 some arbitrary number of times. Instead we can get rid
+ * of all duplicate 2 factors by dividing by 4 a single time. This implies that all common factors of m and n will appear in
+ * their greatest common divisor, thus we can get rid of all of all common duplicate factors by dividing by the GCD. Therefore
+ * LCM = m*n/gcd(m, n).
+ *
+ * Before moving onto the GCD problem I want to extend the inclusion-exclusion idea with LCM a bit further and show from
+ * a different angle how this works with unique prime factorizations. As shown earlier, all common factors of two numbers
+ * are found in their GCD, so let's see how that looks with their unique prime factorizations.
+ *
+ * Prime factorization of 8:  2^3
+ * Prime factorization of 48: 2^4 * 3^1
+ * When we multiply 8*48 we get 2^4 * 2^3 * 3^1. It is obvious that both 8 and 48 share a factor of 8 and that factor of 8 is duplicated
+ * in 2^4 * 2^3 * 3^1. We can remove it by getting rid of 2^3 in our product factorization. This idea can be generalized to show that the
+ * LCM is product of the largest power of each prime factor. This makes sense because 2^3 (from 8) is contained in 2^4 (from 48) so why
+ * would we need to include the factors of 2 from 8 if they're already contained in 48? It makes sense that we have to take the largest
+ * power of each prime too since we need to contain the factors of BOTH numbers, thus we're limited by the larger one. It is nice and the
+ * logic is symmetric to what we had earlier. Analyzing each prime individually results in the same duplicate deletion yielded from
+ * dividing m*n by the GCD.
+ *
+ * For the GCD, we're taking advantage of the Euclidian description on Wiki. Yay.
  */
 
- int gcd(int a, int b) {
-   while (a != b) {
-     if (a > b) {
-       a -= b;
-     } else {
-       b -= a;
-     }
-   }
+int gcd(int a, int b) {
 
-   return a;
- }
-
-unsigned long lcm(int m, int n) {
-  //return m / gcd(m, n) * n;
-  // We are guaranteed that m <= n
-  unsigned long lcmRet = m * n;
-
-  // First factor we want to check to see if both have in common
-  int i = m;
-
-  // lcmRet/i >=n because if the next operation could invalidate
-  // lcmRet, we want to retun it now, before it gets invalid.
-  while (i && lcmRet/i >= n && lcmRet <= m*n) {
-    if (m % i == 0 && n % i == 0) {
-      // Alternatively we could check if coming operation will
-      // make lcmRet in valid if so return it while still good
-      if (lcmRet / i < n) return lcmRet;
-      lcmRet /= i;
+  while (a != b) {
+    if (a > b) {
+      a -= b;
+    } else {
+      b -= a;
     }
-    --i;
   }
 
-  return lcmRet;
+  return a;
+}
+
+int lcm(int m, int n) {
+  return m*n/gcd(m, n);
 }
 
 int main() {
