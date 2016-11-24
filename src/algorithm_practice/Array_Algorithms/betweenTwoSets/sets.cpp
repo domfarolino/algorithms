57d50c7abff84a3fa4beee8430f6131aba9e68be
domfarolino <domfarolino@gmail.com>
2016-11-24 13:00:09 -0500

    algo(between-two-sets): started algorithm

---
+++ src/algorithm_practice/Array_Algorithms/betweenTwoSets/sets.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Source: https://www.hackerrank.com/challenges/between-two-sets
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int>, a(n), b(m);
+
+  for (int i = 0; i < a.size(); ++i) {
+    cin >> a[i];
+  }
+
+  for (int i = 0; i < b.size(); ++i) {
+    cin >> b[i];
+  }
+
+  return 0;
+}
