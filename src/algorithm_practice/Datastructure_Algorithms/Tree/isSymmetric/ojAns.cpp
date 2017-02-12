684c538e60ed2c2ca715860d61141e4fffd6c283
domfarolino <domfarolino@gmail.com>
2017-02-12 11:33:45 -0500

    chore(isSymmetric): ojAns source link

--- src/algorithm_practice/Datastructure_Algorithms/Tree/isSymmetric/ojAns.cpp
+++ src/algorithm_practice/Datastructure_Algorithms/Tree/isSymmetric/ojAns.cpp
@@ -1,4 +1,4 @@
-// Source: https://leetcode.com/problems/symmetric-tree/?tab=Solutions
+// Source: https://leetcode.com/problems/symmetric-tree/
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
