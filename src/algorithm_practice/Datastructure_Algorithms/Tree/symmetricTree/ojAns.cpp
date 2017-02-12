63bbc03b16f44dc63bb70d8afc641be27ee8781b
domfarolino <domfarolino@gmail.com>
2017-02-12 00:46:35 -0500

    algo(isSymmetric): added ojAns

---
+++ src/algorithm_practice/Datastructure_Algorithms/Tree/symmetricTree/ojAns.cpp
@@ -0,0 +1,26 @@
+// Source: https://leetcode.com/problems/symmetric-tree/?tab=Solutions
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+      if (!root) return true;
+      if (!root->left && !root->right) return true;
+      if (!root->left || !root->right) return false;
+
+      return subtreesSymmetric(root->left, root->right);
+    }
+
+    bool subtreesSymmetric(TreeNode *left, TreeNode *right) {
+      if (!left && !right) return true;
+      if (!left || !right) return false;
+      return (left->val == right->val) && subtreesSymmetric(left->left, right->right) && subtreesSymmetric(left->right, right->left);
+    }
+};
