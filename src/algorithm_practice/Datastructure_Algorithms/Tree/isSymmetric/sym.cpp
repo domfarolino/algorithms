b1fcc93010e8aa896eeff76228be99fb16874808
domfarolino <domfarolino@gmail.com>
2017-04-16 00:49:08 -0400

    algo(isSymmetric): minor doc changes

--- src/algorithm_practice/Datastructure_Algorithms/Tree/isSymmetric/sym.cpp
+++ src/algorithm_practice/Datastructure_Algorithms/Tree/isSymmetric/sym.cpp
@@ -8,18 +8,18 @@ using namespace std;
  * The goal of this algorithm is to write an algorithm that determines whether
  * or not a tree is symmetric. We'll be taking in a tree root pointer and returning
  * a boolean. We'll be calling the equal operator on the value inside each node so an
- * assumption we'd be making is that the given type of data can has an equal operator.
- * With recursive binary tree problems I've found it helpul to visualize the algorithm
- * running on a tree with zero nodes and a tree whose root has exactly two immediate
- * children. We can say a tree consisting of zero nodes is symmetric. Given a tree with
- * two immediate children we need to make sure
+ * assumption we'd be making is that the given type of data a tree node holds has an
+ * associated `=` operator. With recursive binary tree problems I've found it helpul
+ * to visualize the algorithm running on a tree with zero nodes and a tree whose root
+ * has exactly two immediate children. We can say a tree consisting of zero nodes is
+ * symmetric. Given a tree with two immediate children we need to make sure
  *    1.) The subtree root values are equivalent
  *    2.) The next two subtrees are symmetric.
  *
  * It is clear that two subtrees can only be symmetric if their root values are equal. Once
  * we pass this we must of course compare left->left with right->right and left->right with right->left.
  * We know the first thing (besides base case null pointer checks) we do is compare the values of two subtree
- * roots so we can just re-use our function for the two new subtrees we incurre at each level. This recursion allows
+ * roots so we can just re-use our function for the two new subtrees we incur at each level. This recursion allows
  * us to perform the necessary breadth expansion we need in order to check symmetry!
  *
  * Complexity analysis:
