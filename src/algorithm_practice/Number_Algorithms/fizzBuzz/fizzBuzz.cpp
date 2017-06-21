ff150ff97a26c68b0f5e7940ccddb1320b84fba5
jonathanGB <jonathan.guillotte.blouin@gmail.com>
2017-06-21 01:23:03 -0400

    fix(fizz-buzz): fixed index access in previous commit

--- src/algorithm_practice/Number_Algorithms/fizzBuzz/fizzBuzz.cpp
+++ src/algorithm_practice/Number_Algorithms/fizzBuzz/fizzBuzz.cpp
@@ -10,10 +10,10 @@ vector<string> fizzBuzz(int n) {
   vector<string> returnVec(n, "");
 
   for (int i = 1; i <= n; ++i) {
-    if (i % 3 == 0 && i % 5 == 0) returnVec[i] = "FizzBuzz";
-    else if (i % 3 == 0) returnVec[i] = "Fizz";
-    else if (i % 5 == 0) returnVec[i] = "Buzz";
-    else returnVec[i] = to_string(i);
+    if (i % 3 == 0 && i % 5 == 0) returnVec[i - 1] = "FizzBuzz";
+    else if (i % 3 == 0) returnVec[i - 1] = "Fizz";
+    else if (i % 5 == 0) returnVec[i - 1] = "Buzz";
+    else returnVec[i - 1] = to_string(i);
   }
 
   return returnVec;
