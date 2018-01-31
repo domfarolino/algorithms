c0ece4ac7269b6b29ec7a4d417f4dd6e8318f5d4
domfarolino <domfarolino@gmail.com>
2018-01-31 00:21:11 -0500

    docs(selfDividingNumbers): add final docs

--- src/algorithm_practice/Number_Algorithms/selfDividingNumbers/self.cpp
+++ src/algorithm_practice/Number_Algorithms/selfDividingNumbers/self.cpp
@@ -17,9 +17,22 @@
  * we're testing. We'll check to see if the given number is divisible
  * by tmp % 10 if it ever isn't, we'll want to return false since we found
  * some digit inside the given number that our given number is not divisible
- * by.
+ * by. The catch (something I first did not consider) how to handle our tmp variable
+ * when it is equal to 0. For example, if a given number was `208`, eventually our tmp
+ * will be `20`, and we'll be testing to see if `num % (tmp % 10)`. In this case however
+ * `tmp % 10` is 0 since our reduced tmp variable is divisible by 10. In this case we'll
+ * get a divide-by-zero error when evaluating `num % 0`. In this case we want to catch the
+ * case where `tmp % 10` early so we don't accidentally dip into a situation where we divide
+ * by zero. This is nice because it provides the behavior that we might expect if we didn't
+ * expect `(num % (tmp % 10))` to throw an error. Of course any number that has a 0 in its digits
+ * is not a self-dividing number.
  *
- * TODO(domfarolino): write about complexity and random edge case
+ * Time complexity: O(left - right)
+ *   For each (left - right) number we perform the isSelfDividingNumber. The isSelfDividingNumber check
+ *   performs O(log(k)) steps where `k` is the given number. Since we know the maximum number in our range
+ *   (being 10000) this makes the complexity of this routine O(log(10000)), which is O(1) since we're dealing
+ *   with a constant.
+ * Space complexity: O(left - right)
  */
 
 bool isSelfDividingNumber(int num) {
