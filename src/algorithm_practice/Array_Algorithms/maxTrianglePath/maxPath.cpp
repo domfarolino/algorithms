971ccd8981829d2fb1dba856a8c504e7a3d961b8
Dominic Farolino <domfarolino@gmail.com>
2018-04-14 22:14:29 -0400

    fix(maxTrianglePath): fix array bounds error from previous issue. Closes #80

--- src/algorithm_practice/Array_Algorithms/maxTrianglePath/maxPath.cpp
+++ src/algorithm_practice/Array_Algorithms/maxTrianglePath/maxPath.cpp
@@ -2,8 +2,6 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
 // Source: Yodle careers
 
 /**
@@ -25,45 +23,46 @@ using namespace std;
  * Space complexity: O(h) height of the triangle
  *
  * The iterative solution is more efficient and stems from our need to save
- * and reuse data we've already calculated. This is exactly how dynamica programming
+ * and reuse data we've already calculated. This is exactly how dynamic programming
  * works, and in this case it is easy to implement because we have the overlapping
  * subproblem property as well as the optimal substructure given by our recursive definition.
  * What we can do is build a two dimensional array the same size as our input and call it memo.
  * memo[r][c] would contain the maximum sum possible you could attain while ending at point [r][c].
  * We can build our two dimensional vector from the top down using "tabulation". Our new iterative
- * definition becomes: memo[r][c] = triangle[r][c] + max(memo[r-1][c], memo[r-1][c-1]). memo[0][0]
- * is equivalent to triangle[0][0] of course. Our return value will be the largest value in our memo
- * or if the triangle allows negative numbers, the largest memo value in the last row.
+ * definition becomes: memo[r][c] = triangle[r][c] + max(memo[r-1][c], memo[r-1][c-1]). memo[0][0] *
+ * is equivalent to triangle[0][0] of course. Our return value will be the largest value in our memo or
+ * if the triangle allows negative numbers, the largest memo value in the last row.
  * Time complexity: O(n)
  * Space complexity: O(n)
  */
 
 // Utility function to print triangle
-void print2DVector(const vector<vector<unsigned long> >& vec) {
+void print2DVector(const std::vector<std::vector<unsigned long> >& vec) {
   for (int i = 0; i < vec.size(); ++i) {
     for (int j = 0; j <= i; ++j) {
-      cout << vec[i][j] << " ";
+      std::cout << vec[i][j] << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
   }
 }
 
 int main() {
   int numRows;
-  cin >> numRows;
+  std::cin >> numRows;
   // Assuming no triangle values are less than 0
-  vector<vector<unsigned long> > triangle(numRows, vector<unsigned long>());
+  std::vector<std::vector<unsigned long> > triangle(numRows, std::vector<unsigned long>());
 
-  for (int r = 0; r < numRows; ++r) {
-    triangle[r].resize(r+1);
-    for (int c = 0; c <= r; ++ c) {
-      cin >> triangle[r][c];
+  for (int r = 0; r < numRows; r++) {
+    triangle[r].resize(r + 1);
+    for (int c = 0; c <= r; c++) {
+      std::cin >> triangle[r][c];
       if (!r && !c) continue;
       else if (!c) triangle[r][c] += triangle[r-1][c];
-      else triangle[r][c] += max(triangle[r-1][c], triangle[r-1][c-1]);
+      else if (c == r) triangle[r][c] += triangle[r-1][c-1];
+      else triangle[r][c] += std::max(triangle[r-1][c], triangle[r-1][c-1]);
     }
   }
 
-  cout << "maxPathSum: " << *max_element(triangle[triangle.size()-1].begin(), triangle[triangle.size()-1].end()) << endl;
+  std::cout << "maxPathSum: " << *max_element(triangle[triangle.size()-1].begin(), triangle[triangle.size()-1].end()) << std::endl;
   return 0;
 }
