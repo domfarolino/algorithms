4cf07b6aaebe19982c816b255819e1e7842876c4
Dominic Farolino <domfarolino@gmail.com>
2020-01-01 22:58:34 -0500

    Re-order functions in min.cc

--- src/algorithm_practice/Array_Algorithms/minimumLoss/min.cc
+++ src/algorithm_practice/Array_Algorithms/minimumLoss/min.cc
@@ -5,10 +5,37 @@
 #include <string>
 #include <set>
 
-std::vector<std::string> split_string(std::string);
+// Hackerrank-provided.
+std::vector<std::string> split_string(std::string input_string) {
+  std::string::iterator new_end = std::unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+    return x == y and x == ' ';
+  });
 
-// Complete the minimumLoss function below.
-long minimumLoss(const std::vector<long>& prices) {
+  input_string.erase(new_end, input_string.end());
+
+  while (input_string[input_string.length() - 1] == ' ') {
+    input_string.pop_back();
+  }
+
+  std::vector<std::string> splits;
+  char delimiter = ' ';
+
+  size_t i = 0;
+  size_t pos = input_string.find(delimiter);
+
+  while (pos != std::string::npos) {
+    splits.push_back(input_string.substr(i, pos - i));
+
+    i = pos + 1;
+    pos = input_string.find(delimiter, i);
+  }
+
+  splits.push_back(input_string.substr(i, std::min(pos, input_string.length()) - i + 1));
+  return splits;
+}
+
+// Complete the minimum_loss function below.
+long minimum_loss(const std::vector<long>& prices) {
   std::set<long> vals;
   long min_difference = LONG_MAX;
   std::set<long>::iterator it;
@@ -43,35 +70,7 @@ int main() {
     price[i] = price_item;
   }
 
-  long result = minimumLoss(price);
+  long result = minimum_loss(price);
   std::cout << result << std::endl;
   return 0;
-}
-
-std::vector<std::string> split_string(std::string input_string) {
-  std::string::iterator new_end = std::unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-    return x == y and x == ' ';
-  });
-
-  input_string.erase(new_end, input_string.end());
-
-  while (input_string[input_string.length() - 1] == ' ') {
-    input_string.pop_back();
-  }
-
-  std::vector<std::string> splits;
-  char delimiter = ' ';
-
-  size_t i = 0;
-  size_t pos = input_string.find(delimiter);
-
-  while (pos != std::string::npos) {
-    splits.push_back(input_string.substr(i, pos - i));
-
-    i = pos + 1;
-    pos = input_string.find(delimiter, i);
-  }
-
-  splits.push_back(input_string.substr(i, std::min(pos, input_string.length()) - i + 1));
-  return splits;
 }
